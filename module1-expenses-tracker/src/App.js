39eeb15695df2bd266edde2d2d33c3fd7a6efb06
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2023-04-09 15:02:23 +0200

    adding form with states

--- module1-expenses-tracker/src/App.js
+++ module1-expenses-tracker/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
 function App() {
   const expenses = [
     {
@@ -37,7 +39,7 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
