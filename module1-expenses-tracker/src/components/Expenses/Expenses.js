605153f8e50a63acbd855c76697957ba9e814ffb
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2023-04-12 18:20:18 +0200

    adding filtered expenses to new component

--- module1-expenses-tracker/src/components/Expenses/Expenses.js
+++ module1-expenses-tracker/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filterYear, setFilterYear] = useState("2020");
@@ -18,27 +18,13 @@ function Expenses(props) {
     );
   };
 
-  let content = (
-    <p style={{ color: "white", fontSize: "20px" }}>No Expenses!!</p>
-  );
-
-  if (filteredContent.length > 0) {
-    content = filteredContent.map((expense) => (
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        key={expense.id}
-      />
-    ));
-  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedYear={filterYear}
         onChangingSelectValue={selectValueHandler}
       />
-      {content}
+      <ExpensesList items={filteredContent} />
     </Card>
   );
 }
