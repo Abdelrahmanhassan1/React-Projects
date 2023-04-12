40e8215caff9cd0a9f138933c2f91821bb3fedce
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2023-04-12 14:24:55 +0200

    adding year filter

--- module1-expenses-tracker/src/components/Expenses/Expenses.js
+++ module1-expenses-tracker/src/components/Expenses/Expenses.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filterYear, setFilterYear] = useState("2020");
+
+  const [filteredContent, setFilteredContent] = useState(props.expenses);
+
+  const selectValueHandler = (selectValue) => {
+    setFilterYear(selectValue);
+    setFilteredContent(
+      props.expenses.filter(
+        (expense) => expense.date.getFullYear().toString() === selectValue
+      )
+    );
+  };
   return (
     <Card className="expenses">
-      {props.expenses.map((expense) => (
+      <ExpensesFilter
+        selectedYear={filterYear}
+        onChangingSelectValue={selectValueHandler}
+      />
+
+      {filteredContent.map((expense) => (
         <ExpenseItem
           title={expense.title}
           amount={expense.amount}
